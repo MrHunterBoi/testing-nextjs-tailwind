6487472f301d5d6d5419e38e8dc9a7ae29e04d2f
MrHunterBoi <m.svoboda.corp@gmail.com>
2024-11-18 16:52:42 +0200

    Changed landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className='antialiased'>
+      <body className='antialiased min-h-screen'>
         <Header />
 
-        <div className='m-auto max-w-6xl'>{children}</div>
+        <div className='m-auto max-w-6xl p-4'>{children}</div>
       </body>
     </html>
   );
